1d2368ee19b2058af49a46d9debb17d979c85fb3
ignatovich01 <nik.ignatovich2001@gmail.com>
2026-02-07 02:02:16 +0300

    add header

---
+++ src/components/Header/TopMenu.tsx
@@ -0,0 +1,64 @@
+import {
+  HStack,
+  Text,
+  MenuRoot,
+  MenuTrigger,
+  Image,
+  MenuItem,
+  MenuContent,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { Polygon } from "../../assets";
+
+export const TopMenu = () => {
+  const [active, setActive] = useState<"Заявки" | "Отчеты" | "Справочники">(
+    "Заявки",
+  );
+
+  const color = (v: string) => (active === v ? "black" : "gray.500");
+  const weight = (v: string) => (active === v ? "600" : "400");
+
+  return (
+    <HStack gap="20px">
+      <Text
+        cursor="pointer"
+        color={color("Заявки")}
+        fontWeight={weight("Заявки")}
+        onClick={() => setActive("Заявки")}
+      >
+        Заявки
+      </Text>
+
+      <Text
+        cursor="pointer"
+        color={color("Отчеты")}
+        fontWeight={weight("Отчеты")}
+        onClick={() => setActive("Отчеты")}
+      >
+        Отчеты
+      </Text>
+
+      <MenuRoot>
+        <MenuTrigger asChild>
+          <HStack
+            cursor="pointer"
+            gap="2"
+            color={color("Справочники")}
+            fontWeight={weight("Справочники")}
+            onClick={() => setActive("Справочники")}
+          >
+            <HStack spaceX="4px">
+              <Text fontWeight="600">Справочники</Text>
+              <Image
+                src={Polygon}
+                alt="стрелка"
+                boxSize="7px"
+                objectFit="contain"
+              />
+            </HStack>
+          </HStack>
+        </MenuTrigger>
+      </MenuRoot>
+    </HStack>
+  );
+};
---
+++ src/components/MainTable.tsx
@@ -0,0 +1,3 @@
+export const MainTable = () => {
+  return <div>MainTable</div>;
+};
